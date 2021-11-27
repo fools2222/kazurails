cfa0f371c235f67111e0b0f2242ae8a4e42fd976
fools <foolishnes141414@gmail.com>
2021-11-27 14:06:40 +0900

    bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'answers/edit'
+  # get 'answers/edit'
   # get 'questions/index'
   # get 'blogs/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
