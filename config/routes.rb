a44c58ff1604022a9d5e72fe23003b3b2da8b18c
fools <foolishnes141414@gmail.com>
2022-01-06 22:40:48 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "mainpages#index"
-  resources :mainpages 
+
+  resources :mainpages do
     resources :questions do 
       resources :answers
     end
+  end
 end
