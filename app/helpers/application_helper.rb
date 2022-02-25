8a0cab3627d1c8ddba9145200c56e0cb3567ad9b
fools <foolishnes141414@gmail.com>
2022-02-25 20:41:05 +0900

    meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+    def default_meta_tags
+        {
+            site:"kazutter",
+            title:"kazutter",
+            reverse: true,
+            charset: "utf-8",
+            description: 'description',
+            keywords: 'kazutter',
+            canonical: request.original_url,
+            separator: '|',
+            icon: [
+                { href: image_url('favicon.ico') },
+                { href: image_url('sora.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+            ],
+            og: {
+                site_name: "kazutter", 
+                title: 'kazutter',
+                description: 'description',
+                type: 'website',
+                url: request.original_url,
+                image: image_url('sora.jpg'),
+                locale: 'ja_JP',
+            },
+            
+        }
+    end
 end
