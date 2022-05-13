55b03cbcc5625e4762d18a74646c1cfb45953b4c
fools <foolishnes141414@gmail.com>
2022-05-13 21:59:13 +0900

    code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,6 @@ class ArticlesController < ApplicationController
   def index
     @articles = view_context.contentful.entries(content_type: 'kazutter')
     
-    render html: "hello world"
-    
   end
 
   def show
